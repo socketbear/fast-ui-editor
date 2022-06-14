77dfc704c7a4de9744f35c73fb1fe3a03e68646a
장동현 <jgbossassa@iabacus.co.kr>
2022-06-14 16:09:47 +0900

    prisma 라이브러리 추가 및 샘플 작성 (실패)
    git ignore 항목 추가

---
+++ pages/sandbox/api.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-2/3 mx-auto">
+    <p class="text-2xl font-bold">API TEST</p>
+    <div>
+      <div class="flex">
+        <p class="font-bold mr-12">list</p>
+        <button @click="getProjects">가져오기</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const project = reactive({
+      list: []
+    });
+    const getProjects = async () => {
+      const controller = new AbortController()
+      // 5 second timeout:
+      const timeoutId = setTimeout(() => controller.abort(), 3000)
+
+      const prjs = await $fetch('/api/project', { signal: controller.signal })
+      console.log('prjs :>> ', prjs);
+    }
+    return { getProjects }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ server/api/project.get.ts
@@ -0,0 +1,8 @@
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
+
+export default defineEventHandler(() => {
+  // const prjs = await prisma.project.findMany();
+  // return prjs;
+  return [];
+});
